ad158acbd6918e5d4532b7e37be43bc99701b330
Frank-Pechar <93831849+Frank-Pechar@users.noreply.github.com>
2023-08-13 16:07:04 -0600

    Fixes and vendor prefixing

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -146,13 +146,13 @@ class ProductList extends Component {
     this.#products = [
       new Product(
         'A Pillow',
-        'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
+        '../assets/img/pillow.jpg',
         'A soft pillow!',
         19.99
       ),
       new Product(
         'A Carpet',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Ardabil_Carpet.jpg/397px-Ardabil_Carpet.jpg',
+        '../assets/img/carpet.jpg',
         'A carpet which you might like - or not.',
         89.99
       ),
